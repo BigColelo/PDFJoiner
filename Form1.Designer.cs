c36876bb6b29d0afef9563395c6cc563fb4aa048
BigColelo <filpiriccardo@outlook.it>
2024-09-18 19:02:38 +0200

    Aggiungere i file di progetto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace PDFJoiner
+{
+    partial class PdfJoiner
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(PdfJoiner));
+            this.label1 = new System.Windows.Forms.Label();
+            this.aggiungi = new System.Windows.Forms.Button();
+            this.rimuovi = new System.Windows.Forms.Button();
+            this.conversione = new System.Windows.Forms.Button();
+            this.listaPercorsi = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial", 16.2F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(6, 9);
+            this.label1.MinimumSize = new System.Drawing.Size(200, 50);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(200, 50);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "PDFJOINER";
+            // 
+            // aggiungi
+            // 
+            this.aggiungi.Font = new System.Drawing.Font("Arial", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.aggiungi.Location = new System.Drawing.Point(12, 254);
+            this.aggiungi.Name = "aggiungi";
+            this.aggiungi.Size = new System.Drawing.Size(36, 34);
+            this.aggiungi.TabIndex = 1;
+            this.aggiungi.Text = "+";
+            this.aggiungi.UseVisualStyleBackColor = true;
+            this.aggiungi.Click += new System.EventHandler(this.aggiungi_Click);
+            // 
+            // rimuovi
+            // 
+            this.rimuovi.Font = new System.Drawing.Font("Arial", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rimuovi.Location = new System.Drawing.Point(44, 254);
+            this.rimuovi.Name = "rimuovi";
+            this.rimuovi.Size = new System.Drawing.Size(36, 34);
+            this.rimuovi.TabIndex = 3;
+            this.rimuovi.Text = "-";
+            this.rimuovi.UseVisualStyleBackColor = true;
+            this.rimuovi.Click += new System.EventHandler(this.rimuovi_Click);
+            // 
+            // conversione
+            // 
+            this.conversione.Location = new System.Drawing.Point(730, 254);
+            this.conversione.Name = "conversione";
+            this.conversione.Size = new System.Drawing.Size(90, 34);
+            this.conversione.TabIndex = 4;
+            this.conversione.Text = "Converti";
+            this.conversione.UseVisualStyleBackColor = true;
+            this.conversione.Click += new System.EventHandler(this.conversione_Click);
+            // 
+            // listaPercorsi
+            // 
+            this.listaPercorsi.FormattingEnabled = true;
+            this.listaPercorsi.ItemHeight = 16;
+            this.listaPercorsi.Location = new System.Drawing.Point(12, 51);
+            this.listaPercorsi.Name = "listaPercorsi";
+            this.listaPercorsi.Size = new System.Drawing.Size(808, 196);
+            this.listaPercorsi.TabIndex = 5;
+            // 
+            // PdfJoiner
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(832, 307);
+            this.Controls.Add(this.listaPercorsi);
+            this.Controls.Add(this.conversione);
+            this.Controls.Add(this.rimuovi);
+            this.Controls.Add(this.aggiungi);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "PdfJoiner";
+            this.Text = "PDFJoiner";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button aggiungi;
+        private System.Windows.Forms.Button rimuovi;
+        private System.Windows.Forms.Button conversione;
+        private System.Windows.Forms.ListBox listaPercorsi;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using PdfSharp;
+using PdfSharp.Pdf;
+using PdfSharp.Pdf.IO;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.Button;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace PDFJoiner
+{
+    public partial class PdfJoiner : Form
+    {
+        public PdfJoiner()
+        {
+            InitializeComponent();
+        }
+
+        //Seleziona i file in pdf e li mostra nella listbox
+        private void aggiungi_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog
+            {
+                Multiselect = true,
+                Filter = "PDF Files (*.pdf)|*.pdf",
+                Title = "Seleziona i file da unire"
+            };
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK) 
+            {
+                //creare un array di percorsi dei file PDF selezionati
+                string[] pdfFiles = openFileDialog.FileNames;
+
+                foreach (var pdfFile in pdfFiles) 
+                {
+                    listaPercorsi.Items.Add(pdfFile);
+                }
+                
+            }
+        }
+        //Bisogna selezionare un valore dalla lista e poi cliccare "-"
+        //Elimina il valore selezionato e aggiorna la listbox
+        private void rimuovi_Click(object sender, EventArgs e)
+        {
+            var elimina = listaPercorsi.SelectedItems[0];
+            if (elimina != null) 
+            {
+                listaPercorsi.Items.Remove(elimina);
+            }
+            foreach (var file in listaPercorsi.Items) 
+            {
+                file.ToString();
+            }
+        }
+
+        //Converte i pdf selezionati
+        //Nomini il file da unire e scegli il percorso
+        private void conversione_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog
+            {
+                Filter = "PDF Files (*.pdf)|*.pdf",
+                Title = "Salva il file PDF unito"
+            };
+
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                string outputFile = saveFileDialog.FileName;
+
+                try
+                {
+                    // Creare un documento vuoto per il PDF di output
+                    PdfDocument outputDocument = new PdfDocument();
+
+                    //Unire i PDF
+                    foreach (string item in listaPercorsi.Items)
+                    {
+
+                        if (File.Exists(item))
+                        {
+                            //Aprire ogni file PDF
+                            PdfDocument inputDocument = PdfReader.Open(item, PdfDocumentOpenMode.Import);
+
+                            // Aggiungere ogni pagina del file PDF all'output
+                            for (int i = 0; i < inputDocument.PageCount; i++)
+                            {
+                                PdfPage page = inputDocument.Pages[i];
+                                outputDocument.AddPage(page);
+                            }
+                        }
+                        else 
+                        {
+                            // Gestisci il caso in cui il file non esiste
+                            MessageBox.Show($"Il file non esiste: {item}", "Errore", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        }
+                    }
+
+                    // Salvare il PDF unito
+                    outputDocument.Save(outputFile);
+                    MessageBox.Show($"PDF unito correttamente in {outputFile}");
+                    listaPercorsi.Items.Clear();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+            else 
+            {
+                MessageBox.Show("Nessun file selezionato");
+            }
+        }
+    }
+}
